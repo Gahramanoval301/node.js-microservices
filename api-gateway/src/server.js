951e82993e5fd1576fc46da4b9663528fa5e62b7
Leman Gahramanova <lemangahramanoval@gmail.com>
2025-07-20 00:02:41 +0400

    finish identity-service

---
+++ api-gateway/src/server.js
@@ -0,0 +1,85 @@
+require("dotenv").config();
+const express = require("express")
+const cors = require("cors")
+const Redis = require("ioredis")
+const helmet = require("helmet")
+const {rateLimit} =require("express-rate-limit")
+const {RedisStore} = require("rate-limit-redis")
+const logger = require("../src/utils/logger")
+const proxy = require("express-http-proxy");
+const errorHandler = require("../src/middleware/errorHandler");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const redisClient = new Redis(process.env.REDIS_URL);
+
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+
+//rate limiting
+const rateLimitOptions = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  heandler: (req, res) => {
+    logger.warn(`Sensitive endpoint rate limit exceeded for IP: ${req.ip}`);
+    res.status(429).json({
+      success: false,
+      message: "Too many requests",
+    });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+app.use(rateLimitOptions);
+
+
+app.use((req, res, next) => {
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.info(`Request body,  ${req.body}`);
+  next();
+});
+
+//proxy 
+// api-gateway -> /v1/auth/register -> 3000
+// identity-service -> /api/auth/register -> 3001 
+
+// so final - localhost:3000/v1/auth/register -> localhost:3001/api/auth/register
+
+const proxyOptions = {
+    proxyReqPathResolver: (req)=>{
+        return req.originalUrl.replace(/^\/v1/, "/api")
+    },
+    proxyErrorHandler: (err, res, next)=>{
+        logger.error(`Proxy error: ${err.message}`);
+        res.status(500).json({
+            message:   `Internal server error`, error: err.message
+        })
+    }
+
+}
+//setting up proxy for our identity service
+app.use('/v1/auth', proxy(process.env.IDENTITY_SERVICE_URL, {
+    ...proxyOptions,
+    proxyReqOptDecorator:(proxyReqOpts, srcReq)=>{
+        proxyReqOpts.headers["Content-Type"] = "application/json"
+        return proxyReqOpts
+    },
+    userResDecorator: (proxyRes, proxyResData, userReq, userRes)=>{
+        logger.info(`Response received from Identity service: ${proxyRes.statusCode}`)
+        return proxyResData;
+    }
+}))
+
+app.use(errorHandler);
+
+app.listen(PORT, ()=>{
+    logger.info(`API Gateway is running on port ${PORT}`)
+    logger.info(`Identity Service is running on port ${process.env.IDENTITY_SERVICE_URL}`)
+    logger.info(`Redis Url ${process.env.REDIS_URL}`)
+})
--- identity-service/src/routes/identity-service.js
+++ identity-service/src/routes/identity-service.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const {registerUser} = require("../controllers/identity-controller")
+const {registerUser, loginUser, logoutUser, refreshTokenUser} = require("../controllers/identity-controller")
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/refresh-token", refreshTokenUser);
+router.post("/logout", logoutUser);
 
 module.exports = router;
